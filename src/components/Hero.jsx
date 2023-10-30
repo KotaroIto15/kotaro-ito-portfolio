56896445cf9cdbef8bb6bd127349cb7f4d3f8213
kotaroito <agkito34@gmail.com>
2023-10-30 00:50:59 -0700

    edited hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,17 +37,11 @@ const Hero = () => {
           <div>
             <h1
               className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Hi, I'm{' '}
-              <span
-                className="sm:text-battleGray sm:text-[90px] 
-                text-eerieBlack text-[50px] font-mova
-                font-extrabold uppercase">
-                
-              </span>
+              Kotaro Ito
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-              Lorem ipsum dolor sit amet. <br className="sm:block hidden" />
-              consectetur adipisicing elit deleniti, voluptas.
+              Third year Computer Science student at <br className="sm:block hidden" />
+              the University of British Columbia.
             </p>
           </div>
           <div
@@ -80,16 +74,6 @@ const Hero = () => {
           </a>
         </div>
 
-        {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
-        {/* <div>
-          <img
-            className="absolute bottom-0 ml-[50vw] 
-            lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
-            sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={shaq}
-            alt="shaquille"
-          />
-        </div> */}
       </section>
     </>
   );
