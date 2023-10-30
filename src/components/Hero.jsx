5cbb59e58b82265f9b07aa5b6f7b84b4b43a9dc5
kotaroito <agkito34@gmail.com>
2023-10-30 00:44:31 -0700

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = () => {
                 className="sm:text-battleGray sm:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
                 font-extrabold uppercase">
-                Shaquille
+                
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
@@ -81,7 +81,7 @@ const Hero = () => {
         </div>
 
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
-        <div>
+        {/* <div>
           <img
             className="absolute bottom-0 ml-[50vw] 
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
@@ -89,7 +89,7 @@ const Hero = () => {
             src={shaq}
             alt="shaquille"
           />
-        </div>
+        </div> */}
       </section>
     </>
   );
